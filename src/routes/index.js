ed8cfa3addaca956c47a2801b6d6b66ac7599542
Hachikoi-the-creator <facka72@gmail.com>
2022-05-13 18:59:45 -0500

    added all post & images

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,9 +9,26 @@ router.get('/donations', (req, res) => {
   res.render('post/donations', { title: 'Donations Page' });
 });
 
+// 2st Post
 router.get('/hamburger-menu-ux', (req, res) => {
   res.render('post/hamburger-menu-ux', { title: 'first post' });
 });
+
+// 2nd post
+router.get('/react-router6-template', (req, res) => {
+  res.render('post/react-router6-template', { title: 'React Router 6' });
+});
+
+// 3rd post
+router.get('/first-os-contribution-local', (req, res) => {
+  res.render('post/first-os-contribution-local', { title: 'OS Tutorial local' });
+});
+
+// 4th post
+router.get('/first-os-contribution-online', (req, res) => {
+  res.render('post/first-os-contribution-online', { title: 'OS Tutorial online' });
+});
+
 
 // router.get('/contact', (req, res) => {
 //   res.render('post/contact', { title: 'Contact Page' });
