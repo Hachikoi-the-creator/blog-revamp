3c9fc3cabf2f3b8650d50452a394b6a42e092be4
Hachikoi-the-creator <facka72@gmail.com>
2022-05-13 17:02:06 -0500

    added burger post & prism

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,16 +5,19 @@ router.get('/', (req, res) => {
   res.render('index', { title: 'First Web Node' });
 });
 
-router.get('/contact', (req, res) => {
-  res.render('post/contact', { title: 'Contact Page' });
-});
-
 router.get('/donations', (req, res) => {
   res.render('post/donations', { title: 'Donations Page' });
 });
 
-router.get('/about', (req, res) => {
-  res.render('post/about', { title: 'About Page' });
+router.get('/hamburger-menu-ux', (req, res) => {
+  res.render('post/hamburger-menu-ux', { title: 'first post' });
 });
+
+// router.get('/contact', (req, res) => {
+//   res.render('post/contact', { title: 'Contact Page' });
+// });
+// router.get('/about', (req, res) => {
+//   res.render('post/about', { title: 'About Page' });
+// });
 
 module.exports = router;
